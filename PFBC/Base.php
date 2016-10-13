bfaf827e004deebd4af81bc4b9d3b3c1d81080e6
Carlos Villaronga <cvillaronga@gmail.com>
2016-10-13 07:30:22 -0400

    PHP 7 Fix Update
    There is a PHP7 Fatal error ->  Uncaught Error: Function name must be a string.
    
    This Update Fix the error

--- PFBC/Base.php
+++ PFBC/Base.php
@@ -27,9 +27,11 @@ abstract class Base {
 					/*If the appropriate class has a "set" method for the property provided, then
 					it is called instead or setting the property directly.*/
 					if(isset($method_reference["set" . $property]))
-						$this->$method_reference["set" . $property]($value);
+						/* update for fix error in php7 enviroment */
+						$this->{$method_reference["set" . $property]}($value);
 					elseif(isset($property_reference[$property]))
-						$this->$property_reference[$property] = $value;
+						/* update for fix error in php7 enviroment */
+						$this->{$property_reference[$property]} = $value;
 					/*Entries that don't match an available class property are stored in the attributes
 					property if applicable.  Typically, these entries will be element attributes such as
 					class, value, onkeyup, etc.*/
